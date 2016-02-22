8c3a29b60fd2a10fc526fe527c2c8d4ac9279c41
tandztc <tandztc@yeah.net>
2016-02-22 13:23:11 +0800

    创建项目文件

---
+++ app/src/main/java/com/tanyong/beatbox/BeatBoxActivity.java
@@ -0,0 +1,12 @@
+package com.tanyong.beatbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeatBoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
